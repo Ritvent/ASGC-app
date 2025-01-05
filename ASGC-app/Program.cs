69d9dd1d23fd569b6dace2b641a4d843b48f8460
Ritvent <ivanventurilloq@gmail.com>
2025-01-06 07:58:41 +0800

    added three functions

---
+++ ASGC-app/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace ASGCapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static void Function1()
+        {
+
+        }
+        static void Function2()
+        {
+
+        }
+        static void Function3()
+        {
+
+        }
+    }
+}
